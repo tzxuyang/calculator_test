09f4ba730dad0588b3169e626ef79bf98753b2ab
yangxu <yang.xu@nio.com>
2023-12-23 16:55:51 +0800

    remove interaction

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 from src.calculator import calculator
 
 if __name__=='__main__':
-    calculator()
+
+    testing_string = "2 + 8,3 * 7,4 / 6,1.2 - 2.4"
+
+    print(testing_string)
+
+    calculator(test_mode='test', test_string = testing_string)
 
 
 
